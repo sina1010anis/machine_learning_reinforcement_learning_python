0f587c10169ec77f4f5ef1ee42c4b6d71adf470b
sina1010anis <sina1010anis@gmail.com>
2024-01-25 18:07:32 +0330

    Section 3 ML

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 import numpy as np
 #---------------START Section 2 ML------------------------
-np_arr = np.array([[1, 2], [3, 4]])
+# np_arr = np.array([[1, 2], [3, 4]])
 
-np_mat = np.matrix([[1, 2], [3, 4]])
+# np_mat = np.matrix([[1, 2], [3, 4]])
 
-mul_normal = np_arr@np_arr
+# mul_normal = np_arr@np_arr
 
-mul_num = np.dot(np_arr, np_arr);
+# mul_num = np.dot(np_arr, np_arr);
 
-mul_num_mul = np.multiply(np_arr, np_arr);
+# mul_num_mul = np.multiply(np_arr, np_arr);
 
-mul_num_pro = np.prod(np_arr);
+# mul_num_pro = np.prod(np_arr);
 
-print(mul_num_pro)
+# print(mul_num_pro)
 
 #---------------END Section 2 ML------------------------
 
+#---------------START Section 3 ML------------------------
+    
+a = np.ones((3, 1))
+
+b = np.array([2, 6, 7])
+
+c = a+b
+
+print(c)
+
+#---------------END Section 3 ML------------------------
